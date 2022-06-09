a55ad7993cf20637ad65f70b0c217ae4bf9e3d46
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-06-09 20:43:21 +0900

    -

--- style.js
+++ style.js
@@ -6,7 +6,10 @@ document.body.insertAdjacentHTML('afterbegin',`<link rel="stylesheet" href="http
 	background-color:var(--bc);
 }
 *{-webkit-tap-highlight-color:#0000;}
-#bg{position:fixed;width:100%;height:100%;z-index:-1;background-size:cover;}
+#bg{position:fixed;width:100%;height:100%;z-index:-1;transition:background 1s;pointer-events:none;background:center/cover;user-select:none;-webkit-user-select:none;}#bgi{width:100vmin;height:auto;float:right;transform:translateX(25%);}
 :root{color:var(--fc);font-family:"M PLUS Rounded 1c",sans-serif;}
 :link{color:var(--b2);}:link:hover{color:var(--b1);}:link:active{color:var(--b0);}:visited{color:var(--p2);}:visited:hover{color:var(--p1);}:visited:active{color:var(--p0);}
+</style>
+<div id="bg"><img id="bgi" src="https://mcbeeringi.github.io/sky_beta/img/icon_.svg" width="1" height="1"></div>
 `);
+addEventListener('DOMContentLoaded',()=>document.body.insertAdjacentHTML('beforeend',``));
