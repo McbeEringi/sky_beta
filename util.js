6b0ea53f900744f66bee333831c9aefc4593059b
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-06-15 19:12:36 +0900

    -

--- util.js
+++ util.js
@@ -2,13 +2,14 @@
 if('serviceWorker'in navigator&&location.protocol.includes('https'))addEventListener('load',()=>navigator.serviceWorker.register('sw.js').then(x=>{console.log('sw Registered',x);}),{once:true});
 localStorage.sky_bgcode||(localStorage.sky_bgcode='linear-gradient(60deg,#214,#415)');
 localStorage.sky_bgmode||(localStorage.sky_bgmode=0);
+localStorage.sky_bgmode
 let idb=indexedDB.open('sky_idb',4),
 	tex=new Image(),
 	texts={
-		back2top:'Back to Top',bgcfg:'Background Config',bgsl:['dynamic','photo','CSS code'],
+		back2top:'Back to Top',bgcfg:'Background Config',bgi:'Background Image',bgil:['Dynamic','Photo','CSS Code'],bgs:'Background Sound',bgsl:['None','Hotspring','Home','Forest','Vault'],
 		...{
 			ja:{
-				back2top:'トップに戻る',bgcfg:'背景設定',bgsl:['ダイナミック','画像','CSSコード']
+				back2top:'トップに戻る',bgcfg:'背景設定',bgi:'背景画像',bgil:['ダイナミック','画像','CSSコード'],bgs:'背景音',bgsl:['なし','温泉','ホーム','雨林','書庫'],
 			}
 		}[navigator.language.slice(0,2)]
 	};
@@ -23,21 +24,26 @@ const bgset=(x=-1)=>{
 	},
 	bgcfg=()=>{
 		const e=alert(`${texts.bgcfg}<hr>
-			<div class="items">
-				<div><input type="radio" name="bgcfg" value="0" id="bgcfg0"><label for="bgcfg0" class="btn" style="--bp:-400% 0;"></label><div>${texts.bgsl[0]}</div></div>
-				<div><input type="radio" name="bgcfg" value="1" id="bgcfg1"><label for="bgcfg1" class="btn" style="--bp:-400% 0;"></label><div>${texts.bgsl[1]}<br>
+			${texts.bgi}
+			<div class="items" style="--items:170px;">
+				<div><input type="radio" name="bgicfg" value="0" id="bgicfg0"><label for="bgicfg0" class="btn" style="--bp:-400% 0;"></label><div>${texts.bgil[0]}</div></div>
+				<div><input type="radio" name="bgicfg" value="1" id="bgicfg1"><label for="bgicfg1" class="btn" style="--bp:-400% 0;"></label><div>${texts.bgil[1]}<br>
 					<button class="btn" style="--bp:-600% -400%;" onclick="this.childNodes[0].click();"><input tabindex="-1" type="file" style="width:100%;height:100%;opacity:0;" accept="image/*" onclick="event.stopPropagation();" onchange="e2p(idbos().put(this.files[0],'bgimg')).then(()=>bgset()).catch(alert);">
 					</button><button class="btn" style="--bp:-400% -300%;" onclick="e2p(idbos().delete('bgimg')).then(()=>bgset()).catch(alert);">
 					</button>
 				</div></div>
-				<div><input type="radio" name="bgcfg" value="2" id="bgcfg2"><label for="bgcfg2" class="btn" style="--bp:-400% 0;"></label><div>${texts.bgsl[2]}<br>
-					<button class="btn bgcedt" style="--bp:-400% -400%;"></button>
+				<div><input type="radio" name="bgicfg" value="2" id="bgicfg2"><label for="bgicfg2" class="btn" style="--bp:-400% 0;"></label><div>${texts.bgil[2]}<br>
+					<button class="btn bgicedt" style="--bp:-400% -400%;"></button>
 				</div></div>
 			</div>
+			${texts.bgs}
+			<div class="items" style="--items:120px;">
+				${texts.bgsl.map((x,i)=>'<label><input type="radio" name="bgscfg" value="'+i+'" id="bgscfg'+i+'"><label for="bgscfg'+i+'" class="btn" style="--bp:-400% 0;"></label>'+x+'</label>').join('')}
+			</div>
 		`);
-		e.querySelector(`input[type=radio][name=bgcfg][value="${localStorage.sky_bgmode}"]`).checked=true;
-		e.querySelectorAll(`input[type=radio][name=bgcfg]`).forEach(x=>x.onchange=()=>(localStorage.sky_bgmode=x.value,bgset()));
-		e.querySelector('.bgcedt').onclick=()=>alert(`<textarea class="input" rows="8" cols="40" oninput="(localStorage.sky_bgmode==2&&(localStorage.sky_bgcode=this.value,bgset()));">${localStorage.sky_bgcode}</textarea>`).querySelector('textarea').focus();
+		e.querySelector(`input[type=radio][name=bgicfg][value="${localStorage.sky_bgmode}"]`).checked=true;
+		e.querySelectorAll(`input[type=radio][name=bgicfg]`).forEach(x=>x.onchange=()=>(localStorage.sky_bgmode=x.value,bgset()));
+		e.querySelector('.bgicedt').onclick=()=>alert(`<textarea class="input" rows="8" cols="40" oninput="(localStorage.sky_bgmode==2&&(localStorage.sky_bgcode=this.value,bgset()));">${localStorage.sky_bgcode}</textarea>`).querySelector('textarea').focus();
 	},
 	getAlert=()=>[...document.querySelectorAll('.alert:not(.fade)>.cont')],
 	rmAlert=(e=getAlert().pop())=>e.parentNode.querySelector('.bg').onclick(),
@@ -51,7 +57,7 @@ document.body.insertAdjacentHTML('afterbegin',`<link rel="stylesheet" href="http
 @keyframes spin{0%{transform:rotate(0deg);}100%{transform:rotate(360deg);}}
 :root,.input,.btn{
 	--bc:#222;--fc:#fff;--l:#fea;--b0:#48f;--b1:#8af;--b2:#aef;--p0:#84f;--p1:#a8f;--p2:#eaf;
-	--g:#3338;--btn:64px;--bp:0 0;
+	--g:#3338;--btn:64px;--bp:0 0;--items:200px;
 	background-color:var(--bc);color:var(--fc);font-family:"M PLUS Rounded 1c",sans-serif;text-shadow:0 0 4px var(--bc);
 }
 *{-webkit-tap-highlight-color:#0000;}
@@ -84,13 +90,12 @@ hr{border:1px solid #fff8;border-radius:1px;backdrop-filter:blur(2px);-webkit-ba
 .clock::before{--bp:-200% -0%;filter:saturate(.3);}.clock::after{content:"";transition:.2s;transform:rotate(var(--rot,45deg));filter:drop-shadow(0 0 2px #aef);background-image:url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18'%3E%3Cpath d='M6 6h1v1h-1z' fill='%23caf0f6'/%3E%3C/svg%3E");}
 .clock.d::after,.d .clock::after{--rot:initial;}
 
-.items{display:grid;max-width:100%;width:100vw;grid-template-columns:repeat(auto-fill,minmax(min(200px,100%),1fr));grid-auto-rows:1fr;grid-gap:8px;}
+.items{display:grid;max-width:100%;width:100vw;grid-template-columns:repeat(auto-fill,minmax(min(var(--items),100%),1fr));grid-auto-rows:1fr;grid-gap:8px;}
 .items::after{content:"";grid-column:1/-1;}
 .items input[type=radio]{position:absolute;opacity:0;pointer-event:none;}
-.items>*{box-sizing:border-box;padding:4px;}
-.items>div{display:flex;justify-content:center;align-items:center;}
-.items>div .btn{--btn:44px;}
-.items>div>.btn{--btn:60px;flex-shrink:0;align-self:start;}
+.items>*{box-sizing:border-box;padding:4px;display:flex;align-items:center;}
+.items>* .btn{--btn:44px;}
+.items>*>.btn{--btn:60px;flex-shrink:0;align-self:start;}
 </style>
 <div id="bg"><img id="bgi" src="img/icon_.svg" width="1" height="1"></div>
 `);
